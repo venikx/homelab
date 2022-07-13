fa18f2117f793c51da12d37feffac9127b7c0bbf
Kevin Rangel <code@venikx.com>
2022-07-13 03:48:19 +0300

    feat: add docker virtualization and add to user

--- hosts/mirage/default.nix
+++ hosts/mirage/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 let
   hostname = "mirage";
@@ -6,18 +6,34 @@ in {
   imports = [
     ../all.nix
     ./hardware-configuration.nix
+    ../../containers/homer
   ];
 
   networking = {
     hostName = hostname;
+    firewall = {
+      allowedTCPPortRanges = [{ from = 8000; to = 8100; }];
+    };
   };
+
+  virtualisation = {
+    docker = {
+      enable = true;
+      autoPrune.enable = true;
+      enableOnBoot = false;
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    docker-compose
+  ];
 
   users = {
     mutableUsers = false;
     users.venikx = {
       isNormalUser = true;
       password = "v3nikx";
-      extraGroups = [ "wheel" ];
+      extraGroups = [ "wheel" "docker" ];
     };
   };
 }
