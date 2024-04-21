3e93623a181d35086aba796db50b9aa90f820465
Kevin Rangel <code@venikx.com>
2024-04-21 16:12:11 +0200

    chore: add build script for chakra

--- configure-machines.sh
+++ configure-machines.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env sh
 
-scp -r [!.]* venikx@192.168.1.197:/home/venikx/homelab
+#scp -r [!.]* venikx@192.168.1.197:/home/venikx/homelab
+
+echo "Rebuilding:" $CHAKRA_IP
+nixos-rebuild switch --fast --flake .#chakra \
+    --target-host $CHAKRA_IP \
+    --build-host $CHAKRA_IP \
+    --option eval-cache false
